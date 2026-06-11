9a73281cab94dc6a1f945f0347cce4921d48b0c8
Matthew Williamson <matt.t.williamson@gmail.com>
2026-06-11 14:10:22 -0600

    export the recipe list

---
+++ recipes/init.lua
@@ -0,0 +1,13 @@
+local blacksmithing = require 'recipes/blacksmithing'
+local fletching  = require 'recipes/fletching'
+local poisons = require 'recipes/poisons'
+local radix = require 'recipes/radix'
+local research = require 'recipes/research'
+
+return {
+    ["Blacksmithing"] = blacksmithing,
+    ["Fletching"] = fletching,
+    ["Poisons"] = poisons,
+    ["Radix"] = radix,
+    ["Research"] = research,
+}
